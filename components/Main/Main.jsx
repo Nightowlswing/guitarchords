2641a47519714780edfd8faeffbb1ca2ddc95bc4
Vlad <nightowlswing@gmail.com>
2020-07-13 14:21:31 +0300

    build fix

--- components/Main/Main.jsx
+++ components/Main/Main.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import '../../Styles/global.css'
-import Slider from './Slider'
 import '../../Styles/MainStyles/MainStyle.css'
 import SongList from '../../components/Songs/SongList'
 
@@ -8,9 +7,6 @@ export default class Main extends Component{
     render(){
         return( 
             <div>
-              <div className = 'slides'>
-              <Slider/>
-              </div>
               <div >
                 
               <div >
@@ -56,26 +52,3 @@ const slideImages = [
     arrows: true
   }
    
-  const Slideshow = () => {
-      return (
-        <div className="slide-container">
-          <Slide {...properties}>
-            <div className="each-slide">
-              <div style={{'backgroundImage': `url(${slideImages[0]})`}}>
-                <span>Slide 1</span>
-              </div>
-            </div>
-            <div className="each-slide">
-              <div style={{'backgroundImage': `url(${slideImages[1]})`}}>
-                <span>Slide 2</span>
-              </div>
-            </div>
-            <div className="each-slide">
-              <div style={{'backgroundImage': `url(${slideImages[2]})`}}>
-                <span>Slide 3</span>
-              </div>
-            </div>
-          </Slide>
-        </div>
-      )
-  }
